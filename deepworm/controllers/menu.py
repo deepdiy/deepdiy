e4f3507c58bb7c4deed5a1b78d4fe3a4afc65d2d
DESKTOP-8N9DAID\lab_pc <webrcv@163.com>
2019-03-15 13:06:47 +1100

    feat(control): add on_state function for menu togglebutton

---
+++ deepworm/controllers/menu.py
@@ -0,0 +1,12 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+
+
+class Menu(BoxLayout):
+    current_state=StringProperty('Open')
+    def __init__(self,**kwargs):
+        super(Menu, self).__init__(**kwargs)
+
+    def on_state(self, togglebutton):
+        if togglebutton.state=='down':
+            current_state=togglebutton.text
