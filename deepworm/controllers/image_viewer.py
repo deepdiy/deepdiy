10d5a28afee4e2de3256b32cd48f0130088cc2d4
DESKTOP-8N9DAID\lab_pc <webrcv@163.com>
2019-03-17 14:50:44 +1100

    refactor(control):image_viewer input image by ObjProperty

--- deepworm/controllers/image_viewer.py
+++ deepworm/controllers/image_viewer.py
@@ -6,58 +6,55 @@ sys.path.append('../')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.properties import ListProperty
+from kivy.properties import DictProperty
 from kivy.graphics.texture import Texture
 from controllers.zoom import Zoom
-
-import cv2
-from array import array
 from kivy.graphics import Rectangle
 
+
+
 class ImageViewer(BoxLayout):
-    img=ListProperty()
+    img=DictProperty()
+    Builder.load_file('../views/image_viewer.kv')
     def __init__(self,**kwargs):
         super(ImageViewer, self).__init__(**kwargs)
-
-        img=cv2.imread('D:\onedrive\program\worm_analyst/training\mask_rcnn\Mask_RCNN\dataset/train\img/training (9).tif')
-        self.h,self.w,_=img.shape
-        # create a 64x64 texture, defaults to rgb / ubyte
-        self.texture = Texture.create(size=(self.w, self.h), colorfmt='rgb')
-        # self.texture.wrap='clamp_to_edge'
-        self.texture.blit_buffer(img.tostring(), colorfmt='bgr', bufferfmt='ubyte')
-        self.texture.flip_vertical()
 
         self.ids = {child.id:child for child in self.children}
         self.bind(size=self.update)
         # print(self.children[0].children[1].children[0])
         self.zoom=self.children[0].children[0].children[0]
-        self.update()
 
+    def img2texture(self):
+        self.h,self.w,_=self.img['img'].shape
+        self.texture = Texture.create(size=(self.w, self.h), colorfmt='rgb')
+        self.texture.blit_buffer(self.img['img'].tostring(), colorfmt='bgr', bufferfmt='ubyte')
+        self.texture.flip_vertical()
+        w,h=self.size
+        if w*h==0:
+            self.w_out,self.h_out=self.size
+        elif w/h>self.w/self.h:
+            self.h_out=h
+            self.w_out=h*(self.w/self.h)
+        else:
+            self.h_out=w*(self.h/self.w)
+            self.w_out=w
 
     def update(self, *args):
+        if self.img=={}:
+            return
+        self.img2texture()
         self.zoom.canvas.clear()
         with self.zoom.canvas:
-            print(self.w,self.h,self.size)
-            w,h=self.size
-            if w*h==0:
-                w_out,h_out=self.size
-            elif w/h>self.w/self.h:
-                h_out=h
-                w_out=h*(self.w/self.h)
-            else:
-                h_out=w*(self.h/self.w)
-                w_out=w
-            Rectangle(texture=self.texture, pos=(0, 0), size=(w_out,h_out))
-            # Rectangle(texture=self.texture, pos=(0, 0), size=self.size)
-        print(self.size)
-
-
+            Rectangle(texture=self.texture, pos=(0, 0), size=(self.w_out,self.h_out))
 
 
 class TestApp(App):
-    Builder.load_file('../views/image_viewer.kv')
     def build(self):
-        return ImageViewer()
+        import cv2
+        img=cv2.imread('D:\onedrive\program\worm_analyst/training\mask_rcnn\Mask_RCNN\dataset/train\img/training (9).tif')
+        image_viewer=ImageViewer()
+        image_viewer.img={'img':img}
+        return image_viewer
 
 if __name__ == '__main__':
     TestApp().run()
