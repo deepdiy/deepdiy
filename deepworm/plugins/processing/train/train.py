8fe411831dbaadd077860ec0519e672146196b25
DESKTOP-8N9DAID\lab_pc <webrcv@163.com>
2019-03-30 22:32:42 +1100

    feat(plugin-train):add spinner for selecting model

--- deepworm/plugins/processing/train/train.py
+++ deepworm/plugins/processing/train/train.py
@@ -5,6 +5,7 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import DictProperty
 from utils.get_parent_path import get_parent_path
+from plugins.processing.networks.model_collector import ModelCollector
 import webbrowser
 
 
@@ -16,12 +17,15 @@ class Train(BoxLayout):
 
 	def __init__(self):
 		super(Train, self).__init__()
+		self.models=ModelCollector().models
+		self.ids.model_spinner.values=self.models
 
 	def label(self):
 		print('hi')
 		webbrowser.open(self.bundle_dir+os.sep+'via'+os.sep+'via.html')
 
-
+	def train(self):
+		print('train')
 
 
 class Test(App):
