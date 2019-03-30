e26335d8f66fb0e5563ba59572a19d94721b0d04
DESKTOP-8N9DAID\lab_pc <webrcv@163.com>
2019-03-30 22:40:13 +1100

    feat(model-mrcnn):add mrcnn api

--- deepworm/model_zoo/mrcnn/api/get_network.py
+++ deepworm/model_zoo/mrcnn/api/get_network.py
@@ -1,2 +1,48 @@
+import sys,os
+sys.path.append('../../../')
+# sys.path.append('../')
+from utils.get_parent_path import get_parent_path
+bundle_dir=get_parent_path(3)+os.sep+'model_zoo'+os.sep+'mrcnn'
+sys.path.append(bundle_dir)
+from mrcnn.config import Config
+from mrcnn import model as modellib
+
+
+class ElegansConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "elegans"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + elegans
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+config = ElegansConfig()
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+
 def get_network():
-    pass
+    model = modellib.MaskRCNN(mode="inference", model_dir='./',config=config)
+    # model.load_weights(bundle_dir+os.sep+'assets'+os.sep+'elegans.h5', by_name=True)
+    return model
+
+def test():
+    get_network()
+
+if __name__ == '__main__':
+    test()
--- deepworm/model_zoo/mrcnn/api/pre_process.py
+++ deepworm/model_zoo/mrcnn/api/pre_process.py
@@ -1,2 +1,24 @@
-def pre_process():
-    pass
+import sys
+sys.path.insert(0,'../../..')
+import cv2
+import numpy as np
+from utils import get_parent_path,read_img
+
+def pre_process(path):
+	img=read_img.read_img(path)
+	# img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	imgdatas = []
+	# img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_CUBIC)
+	imgdatas.append(img)
+	imgdatas = np.array(imgdatas)
+	# imgdatas = imgdatas.astype('float32')
+	# imgdatas = imgdatas / 255.0
+	return imgdatas
+
+def test():
+	path='../../../img/face.jpg'
+	result=run(path)
+	print(result.shape)
+
+if __name__ == '__main__':
+	test()
