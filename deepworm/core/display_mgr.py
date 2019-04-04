5666e3332d913e57853893c1bd4d3896e92116d6
DESKTOP-8N9DAID\lab_pc <webrcv@163.com>
2019-04-04 17:35:55 +1100

    feat(core-Display_mgr):swich display screen with display_mgr

--- deepworm/core/display_mgr.py
+++ deepworm/core/display_mgr.py
@@ -5,7 +5,7 @@ from kivy.properties import DictProperty
 class DisplayManager(BoxLayout):
 	"""docstring for DisplayManager."""
 
-	data=DictProperty({})
+	data=DictProperty()
 
 	def __init__(self,**kwargs):
 		super(DisplayManager, self).__init__(**kwargs)
@@ -13,10 +13,25 @@ class DisplayManager(BoxLayout):
 		if app!=None:
 			app.bind(data=self.setter('data'))
 			self.bind(data=self.display)
+			self.bind(data=app.widget_manager.ids.resource_tree.children[0].setter('data'))
+			app.widget_manager.ids.resource_tree.children[0].bind(data=self.setter('data'))
 
 	def display(self,*args):
-		if not hasattr(self.data,'selection'):
+		app=App.get_running_app()
+
+		if not hasattr(self.data, 'selection'):
 			return
+		if not 'display' in self.data['selection']['data']:
+			return
+		print(self.data['selection']['data']['content'])
+		app.widget_manager.ids.display_screens.current=self.data['selection']['data']['display']
+		app.widget_manager.ids.display_screens.children[0].children[0].data=self.data['selection']['data']['content']
+
+			# app.widget_manager.ids.display_screens.ids.image_viewer.data=self.data
+		# if self.data['selection']['data']['type']=='file_path':
+		# 	self.img=read_img(self.data['selection']['data']['content'])
+		# else:
+		# 	self.img=self.data['selection']['data']['content']
 
 
 class Test(object):
