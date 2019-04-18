daf30a93bed31afef1e70f8d665974f7f90ba4bf
deepdiy <pubrcv@163.com>
2019-04-18 22:15:46 +1000

    feat(utils-get_file_list):add format filter

--- deepdiy/utils/get_file_list.py
+++ deepdiy/utils/get_file_list.py
@@ -1,17 +1,14 @@
 import glob,os
 
-def get_file_list(path):
+def get_file_list(path,formats=['*']):
 	file_list=[]
-	for fn in glob.glob( path + os.sep + '*.*'):
-		if os.path.isdir(fn):
-			pass
-		else:
-			file_list.append(fn)
+	for format in formats:
+		file_list.extend(glob.glob(path + os.sep + '*.'+format))
 	file_list=sorted(file_list,key=len)
 	return file_list
 
 if __name__ == '__main__':
 	path1='../img/'
 	path2='../img/face.jpg'
-	file_list=get_file_list(path1)
+	file_list=get_file_list(path1,formats=['jpg'])
 	print(file_list)
