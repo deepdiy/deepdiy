dd0545f66b71eb1d53abd0b3edb6ee0fb53a2260
deepdiy <pubrcv@163.com>
2019-04-18 21:26:21 +1000

    refacotr(utils-read_img):read img by cv2 without numpy

--- deepdiy/utils/read_img.py
+++ deepdiy/utils/read_img.py
@@ -1,6 +1,10 @@
 import cv2
-import numpy as np
+# import numpy as np
 
 def read_img(path):
-    array=np.fromfile(path,dtype=np.uint8)
-    return cv2.imdecode(array,cv2.IMREAD_COLOR)
+    file_path_gbk = path.encode('gbk')
+    img_mat = cv2.imread(file_path_gbk.decode())
+    return img_mat
+
+    # array=np.fromfile(path,dtype=np.uint8)
+    # return cv2.imdecode(array,cv2.IMREAD_COLOR)
