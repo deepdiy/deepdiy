7caa40122c36407acb97ecc21c0d19bd78091db5
deepdiy <pubrcv@163.com>
2019-04-27 22:28:08 +1000

    feat(plugin-setting):add setting plugin

---
+++ deepdiy/plugins/processing/setting.py
@@ -0,0 +1,27 @@
+import sys,os
+sys.path.append('../../')
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from utils.get_parent_path import get_parent_path
+from kivy.properties import DictProperty
+
+class Setting(BoxLayout):
+	"""docstring for Setting."""
+
+	data=DictProperty()
+	bundle_dir = get_parent_path(3)
+	Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'setting.kv')
+	def __init__(self):
+		super(Setting, self).__init__()
+
+class Test(App):
+	def __init__(self,**kwargs):
+		super(Test, self).__init__(**kwargs)
+
+	def build(self):
+		return Setting()
+
+
+if __name__ == '__main__':
+	test=Test().run()
