460ce9df68fe48ded5f22920388c87974c0fb674
deepdiy <pubrcv@163.com>
2019-04-29 11:44:06 +1000

    refactor(plugin-detection_viewer):render image in thread

--- deepdiy/plugins/display/detection_viewer/detection_viewer.py
+++ deepdiy/plugins/display/detection_viewer/detection_viewer.py
@@ -2,27 +2,29 @@ import sys,os
 sys.path.append('../../../')
 from utils.get_parent_path import get_parent_path
 bundle_dir=get_parent_path(3)+os.sep+'model_zoo'+os.sep+'mrcnn'
+from utils.read_img import read_img
+
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.properties import DictProperty,ObjectProperty
-from utils.read_img import read_img
-from plugins.display.detection_viewer.visualize import random_colors,apply_mask,display_instances
 from kivy.graphics.texture import Texture
+from kivy.graphics import Rectangle
+from kivy.properties import DictProperty,ObjectProperty
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+
+from plugins.display.detection_viewer.visualize import random_colors,apply_mask,display_instances
 import matplotlib
 matplotlib.use('Agg')
-from kivy.graphics import Rectangle
 from matplotlib import pyplot as plt
 import cv2
 import numpy as np
-# import pysnooper
 from pebble import concurrent
-
-from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from functools import partial
 
 class DetectionViewer(BoxLayout):
 	data=DictProperty()
-	img=ObjectProperty()
+	img=DictProperty()
 	w_out,h_out=[640,480]
 	def __init__(self,**kwargs):
 		super(DetectionViewer, self).__init__(**kwargs)
@@ -31,7 +33,8 @@ class DetectionViewer(BoxLayout):
 		self.bind(img=self.draw)
 
 	def img2texture(self):
-		self.h,self.w=self.img.shape[:2]
+		img=self.img['img']
+		self.h,self.w=img.shape[:2]
 		w,h=self.size
 		if w*h==0:
 			self.w_out,self.h_out=self.size
@@ -41,15 +44,21 @@ class DetectionViewer(BoxLayout):
 		else:
 			self.h_out=w*(self.h/self.w)
 			self.w_out=w
-
 		self.texture = Texture.create(size=(self.w, self.h), colorfmt='rgb')
-		self.texture.blit_buffer(self.img.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+		self.texture.blit_buffer(img.tostring(), colorfmt='bgr', bufferfmt='ubyte')
 		self.texture.flip_vertical()
 
 	def update(self, *args):
 		if self.data=={}:
 			return
+		self.add_widget(Label(text='Rendering...Please wait...'))
 		self.data['content']['image']=cv2.cvtColor(self.data['content']['image'],cv2.COLOR_BGR2RGB)
+		render=self.render()
+		render.add_done_callback(lambda job:Clock.schedule_once(partial(self.on_render_finished,job),0.01))
+
+
+	@concurrent.thread
+	def render(self):
 		plt.close('all')
 		fig = plt.figure(frameon=False)
 		ax = fig.add_axes([0, 0, 1, 1])
@@ -63,13 +72,18 @@ class DetectionViewer(BoxLayout):
 		fig.canvas.draw()
 		img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
 		img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+		img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+		return img
 
-		self.img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-		print(self.img.shape)
+	def on_render_finished(self,job,*ars):
+		img=job.result()
+		print(job.result())
+		self.img={'img':img}
 
 	def draw(self,*args):
-		if self.img is None:
+		if len(self.img) ==0:
 			return
+		print(self.img)
 		self.img2texture()
 		print(self.w_out,self.h_out)
 		self.clear_widgets()
