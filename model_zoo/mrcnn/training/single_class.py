f5c31300cbc5f159c8f11b66dc9ed3b484d38e83
deepdiy <pubrcv@163.com>
2019-04-30 23:43:48 +1000

    refactor(model-mrcnn-train):epoch 30->300 in single_class

--- model_zoo/mrcnn/training/single_class.py
+++ model_zoo/mrcnn/training/single_class.py
@@ -208,7 +208,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=300,
                 layers='heads')
 
 
