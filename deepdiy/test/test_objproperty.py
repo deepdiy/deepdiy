fd62bd7ae7d62a61c1ba307ea6958d06248d01a9
deepdiy <pubrcv@163.com>
2019-04-30 15:34:37 +1000

    test(test):test ObjectProperty

---
+++ deepdiy/test/test_objproperty.py
@@ -0,0 +1,64 @@
+import sys,os
+sys.path.append('../')
+from utils.get_parent_path import get_parent_path
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import DictProperty,ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.event import EventDispatcher
+from kivy.uix.button import Button
+
+class Data(EventDispatcher):
+	"""docstring for Data."""
+
+	def __init__(self):
+		super(Data, self).__init__()
+
+
+class Demo(BoxLayout):
+	"""docstring for Demo."""
+
+	bundle_dir = get_parent_path(3)
+	# Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'gallery.kv')
+	data=ObjectProperty(force_dispatch=True)
+	def __init__(self):
+		super(Demo, self).__init__()
+		self.data=Data()
+		self.data.room=0
+		self.bind(data=lambda x,y:print(self.data.room))
+		self.data.room='hi'
+		self.data=self.data
+		self.data.room=1
+		self.property('data').dispatch(self)
+
+class Window(BoxLayout):
+	"""docstring for Window."""
+
+	foo=ObjectProperty(force_dispatch=True)
+	def __init__(self):
+		super(Window, self).__init__()
+		self.demo=Demo()
+		self.demo.bind(data=self.setter('foo'))
+		self.demo.property('data').dispatch(self.demo)
+		self.add_widget(Button(text='pp',on_release=self.update))
+		self.bind(foo=lambda x,y:print(self.foo))
+
+	def update(self,*args):
+		print(self.foo.room)
+		self.demo.data.room+=1
+		# self.foo.room='202'
+
+
+class Test(App):
+	"""docstring for Test."""
+
+	def __init__(self):
+		super(Test, self).__init__()
+
+	def build(self):
+		window=Window()
+		return window
+
+if __name__ == '__main__':
+	Test().run()
