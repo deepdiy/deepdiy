647a1ef3653b18df08f4677ac16a5c8d6a474b9b
deepdiy <pubrcv@163.com>
2019-04-30 16:39:40 +1000

    refactor(plugin-resource_tree):data(dict to obj);auto_select previous

--- deepdiy/plugins/display/resource_tree/resource_tree.py
+++ deepdiy/plugins/display/resource_tree/resource_tree.py
@@ -2,39 +2,34 @@ import os
 from kivy.uix.treeview import TreeView,TreeViewLabel
 from kivy.uix.scrollview import ScrollView
 from kivy.app import App
-from kivy.properties import DictProperty
+from kivy.properties import ObjectProperty
 from kivy.core.window import Window
+import pysnooper
 
 
 class ResourceTree(TreeView):
-    data = DictProperty()
+    data=ObjectProperty(lambda: None)
     def __init__(self, **kwargs):
-        super(ResourceTree, self).__init__(**kwargs)
+        super(ResourceTree, self).__init__()
         self.bind(data=self.update_tree_view)
-        self.size_hint = 1, None
+        self.size_hint_y = None
         self.bind(minimum_height = self.setter('height'))
         self.bind(selected_node = self.update_selection)
         self.hide_root=True
         self.ignore_data_change=False
 
-    def update_selection(self,instance,value):
-        if value ==None:
+    def update_selection(self,instance,node):
+        if node ==None:
             return
         index_chain=[]
-        current=value
+        current=node
         for i in range(100):
             if current.text!='Root':
                 index_chain.insert(0,current.parent_node.nodes.index(current))
                 current=current.parent_node
             else:
                 break
-        selection=self.data['tree']
-        for i in index_chain[1:]:
-            selection=selection['children'][i]
-        if index_chain==[0]:
-            selection=self.data['tree']
-        self.data['selection']={'index_chain':index_chain[1:],'data':selection}
-        self.ignore_data_change=True
+        self.data.select_idx=index_chain
 
     def populate_tree_view(self, parent, node):
         if parent is None:
@@ -45,22 +40,39 @@ class ResourceTree(TreeView):
             self.populate_tree_view(tree_node, child_node)
 
     def depopulate(self,*arg):
+        self.deselect_node()
         for node in self.iterate_all_nodes():
             self.remove_node(node)
 
+    # @pysnooper.snoop()
     def update_tree_view(self,*arg):
-        if not hasattr(self.data, 'tree') or self.ignore_data_change:
-            self.ignore_data_change=False
+        if not hasattr(self.data,'tree'):
             return
         self.depopulate()
-        self.populate_tree_view(None, self.data['tree'])
+        self.populate_tree_view(None, self.data.tree)
+        try:
+            self.auto_select()
+        except:
+            pass
+
+    def auto_select(self):
+        node=self.root
+        for i in self.data.select_idx:
+            node=node.nodes[i]
+        last_child=len(node.nodes)-1
+        if last_child>-1:
+            self.ignore_data_change=True
+            self.select_node(node.nodes[last_child])
+        else:
+            self.ignore_data_change=True
+            self.select_node(node)
 
 
 class TestApp(App):
     def __init__(self):
         super(TestApp, self).__init__()
         self.resource_tree=ResourceTree()
-        self.resource_tree.data={'tree':{'node_id': '1',
+        self.resource_tree.data.tree={'node_id': '1',
             'children': [{'node_id': '1.1',
             'children': [{'node_id': '1.1.1',
             'children': [{'node_id': '1.1.1.1',
@@ -70,7 +82,7 @@ class TestApp(App):
             {'node_id': '1.1.3',
             'children': []}]},
             {'node_id': '1.2',
-            'children': []}]}}
+            'children': []}]}
         self.resource_tree.update_tree_view()
 
     def build(self):
