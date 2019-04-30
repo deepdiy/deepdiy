6053dfd7bbbe9fb3d06a4d51811421b4cdf64467
deepdiy <pubrcv@163.com>
2019-04-30 15:22:08 +1000

    feat(core-data_mgr):add data_mgr to manage data. data is ObjectProperty

---
+++ deepdiy/core/data_mgr.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty,DictProperty
+from pysnooper import snoop
+
+class Data(BoxLayout):
+	"""docstring for Data."""
+
+	select_idx=ListProperty()
+	selected_data=DictProperty()
+	def __init__(self):
+		super(Data, self).__init__()
+		self.tree={'node_id': 'resources', 'children': [{'node_id': '<Path>logo.png', 'content': 'D:/onedrive/program/deepdiy/deepdiy/plugins/processing/gallery/demos/Mask-RCNN+COCO_instance_segmentation\\logo.png', 'display': 'image_viewer', 'children': []}]}
+
+	# @snoop()
+	def get_selected_data(self):
+		self.selected_data=self.tree
+		for i in self.select_idx[1:]:
+			self.selected_data=self.selected_data['children'][i]
+		if self.select_idx==[0]:
+			self.selected_data=self.data.tree
+		return self.selected_data
+
+
+class Test(App):
+	def __init__(self,**kwargs):
+		super(Test, self).__init__(**kwargs)
+
+	def build(self):
+		data=Data()
+		data.select_idx=[0,0]
+		print(data.get_selected_data())
+		return data
+
+if __name__ == '__main__':
+	Test().run()
