c052eb5e03fef6774f7e9b7027ff8f0442907140
deepdiy <pubrcv@163.com>
2019-05-04 16:36:28 +1000

    fix(plugin-train):update train notebook address of train button

--- deepdiy/plugins/processing/train/train.py
+++ deepdiy/plugins/processing/train/train.py
@@ -37,7 +37,7 @@ class Train(BoxLayout):
 		webbrowser.open('http://www.robots.ox.ac.uk/~vgg/software/via/via-2.0.4.html')
 
 	def train(self):
-		webbrowser.open('https://colab.research.google.com/github/deepdiy/deepdiy/blob/master/model_zoo/'+self.ids.run_net_spinner.text+'/training/'+self.ids.run_notebook_spinner.text)
+		webbrowser.open('https://colab.research.google.com/github/deepdiy/deepdiy/blob/master/deepdiy/model_zoo/'+self.ids.run_net_spinner.text+'/training/'+self.ids.run_notebook_spinner.text)
 
 	def select_annotation_path(self):
 		self.annoation_path=select_file()
