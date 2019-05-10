3c1634f096b80b24cb1978fc345ed9792e491256
deepdiy <pubrcv@163.com>
2019-05-10 15:47:36 +1000

    refactor(plugin-networks):replace sandbox with quickplugin

--- deepdiy/plugins/processing/networks/networks.py
+++ deepdiy/plugins/processing/networks/networks.py
@@ -6,7 +6,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty,BooleanProperty
 from plugins.processing.networks.model_collector import ModelCollector
 from utils.get_parent_path import get_parent_path
-from core.sandbox import Sandbox
+from core.quickplugin import QuickPlugin
 from pebble import concurrent
 import json
 
@@ -24,6 +24,11 @@ class Networks(BoxLayout):
 		self.ids.model_spinner.values=self.models
 		self.ids.model_spinner.bind(text=self.update_weight_list)
 		self.ids.model_spinner.bind(text=self.update_config_list)
+
+	def rescan_model_zoo(self):
+		self.models=ModelCollector().models
+		self.ids.model_spinner.text='Select Model'
+		self.ids.model_spinner.values=self.models
 
 	def update_config_list(self,instance,text):
 		if text=='Select Model':
@@ -63,50 +68,39 @@ class Networks(BoxLayout):
 			self.model.config_path=get_parent_path(3)+os.sep+'model_zoo'+os.sep+self.ids.model_spinner.text+os.sep+'configs'+os.sep+self.ids.config_spinner.text
 		else:
 			self.model.config_path=''
-		test=self.load()
-
-	def rescan_model_zoo(self):
-		self.models=ModelCollector().models
-		self.ids.model_spinner.text='Select Model'
-		self.ids.model_spinner.values=self.models
+		self.load()
 
 	@concurrent.thread
 	def load(self):
+		if self.model.config_path!='':
+			self.config=json.load(open(self.model.config_path))
+		else:
+			self.config={'CLASS_NAMES':['background','target']}
 		with self.graph.as_default():
 			self.model.load_network()
 			self.model.load_weight()
 			self.is_weight_loaded=True
 			self.ids.btn_load_weight.text='Load Model'
 
+	def predict(self,selected_data):
+		self.model.set_input(selected_data)
+		return self.model.predict()
+
 	def run(self):
-		selected_data=self.data.get_selected_data()
-		if selected_data['type']!='file_path':
-			return
 		self.ids.btn_run.text='Running'
-		self.model.set_input(selected_data['content'])
-		if self.model.config_path!='':
-			self.config=json.load(open(self.model.config_path))
-		else:
-			self.config={'CLASS_NAMES':['background','target']}
-		self.sandbox=Sandbox(
-			selected_data,
-			use_selected_data=False,
-			graph=self.graph,
-			func=self.model.predict,
-			kwargs={},
-			call_back=self.on_finished,
-			result_meta={
-				'node_id':'mask',
-				'type':'img',
-				'display':'detection_viewer',
-				'class_names':self.config['CLASS_NAMES']
-				}
-			)
-		with self.graph.as_default():
-			self.sandbox.start()
+		quickplugin=QuickPlugin(data=self.data,
+		input_type=['file_path'],
+		result_meta={'node_id':'detection_result',
+			'type':'img',
+			'display':'detection_viewer',
+			'class_names':self.config['CLASS_NAMES']
+			},
+		call_back=self.on_finished,
+		tensorflow_graph=self.graph)
+		quickplugin.run=self.predict
+		quickplugin.start()
 
 	def on_finished(self):
-		print(self.data.get_selected_data())
 		self.property('data').dispatch(self)
 		self.ids.btn_run.text='Run'
 
