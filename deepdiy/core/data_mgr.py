9a1126f7781f9126ef31733588aed2199de355c2
deepdiy <pubrcv@163.com>
2019-05-10 22:09:12 +1000

    feat(core-data_mgr):add load_sample_data()

--- deepdiy/core/data_mgr.py
+++ deepdiy/core/data_mgr.py
@@ -1,3 +1,7 @@
+import sys,os
+sys.path.append('../')
+from utils.get_parent_path import get_parent_path
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty,DictProperty
@@ -6,8 +10,13 @@ from pysnooper import snoop
 class Data(BoxLayout):
 	"""docstring for Data."""
 
+	file_list=ListProperty()
 	select_idx=ListProperty()
+	# tree=DictProperty()
 	selected_data=DictProperty()
+
+	bundle_dir = get_parent_path(3)
+
 	def __init__(self):
 		super(Data, self).__init__()
 
@@ -17,8 +26,23 @@ class Data(BoxLayout):
 		for i in self.select_idx[1:]:
 			self.selected_data=self.selected_data['children'][i]
 		if self.select_idx==[0]:
-			self.selected_data=self.data.tree
+			self.selected_data=self.tree
 		return self.selected_data
+
+	def load_sample_data(self):
+		self.select_idx=[0,0]
+		self.tree={
+			'node_id':'root',
+			'type':None,
+			'content':'',
+			'display':None,
+			'children':[{
+				'node_id':'face.jpg',
+				'type':'file_path',
+				'content':os.sep.join([self.bundle_dir,'img','face.jpg']),
+				'display':None,
+				'children':[]
+			}]}
 
 
 class Test(App):
@@ -27,7 +51,7 @@ class Test(App):
 
 	def build(self):
 		data=Data()
-		data.select_idx=[0,0]
+		data.load_sample_data()
 		print(data.get_selected_data())
 		return data
 
