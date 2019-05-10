6a0cf30acde90cc75342aaea1071d7d0f095fa82
deepdiy <pubrcv@163.com>
2019-05-10 23:27:43 +1000

    fix(plugin-image_viewer): fix texture don't update,by adding kivy clock

--- deepdiy/plugins/display/image_viewer.py
+++ deepdiy/plugins/display/image_viewer.py
@@ -6,19 +6,20 @@ from kivy.properties import DictProperty
 from kivy.graphics.texture import Texture
 from kivy.graphics import Rectangle
 from utils.read_img import read_img
+import cv2
+from kivy.clock import Clock
+from functools import partial
 
 
 class ImageViewer(BoxLayout):
 	data=DictProperty()
 	def __init__(self,**kwargs):
 		super(ImageViewer, self).__init__(**kwargs)
-		# self.add_widget(FigureCanvasKivyAgg(plt.gcf()))
 		self.bind(size=self.update)
 		self.bind(data=self.update)
 
-		# self.add_widget(Image(size=(32,32),texture=self.texture,keep_ratio=True,allow_stretch=False))
 
-	def img2texture(self):
+	def img2texture(self,*arg):
 		self.h,self.w=self.img.shape[:2]
 		self.texture = Texture.create(size=(self.w, self.h), colorfmt='rgb')
 		self.texture.blit_buffer(self.img.tostring(), colorfmt='bgr', bufferfmt='ubyte')
@@ -36,9 +37,17 @@ class ImageViewer(BoxLayout):
 	def update(self, *args):
 		if self.data=={}:
 			return
-		self.img=read_img(self.data['content'])
-		self.img2texture()
+		if self.data['type']=='file_path':
+			self.img=read_img(self.data['content'])
+		elif self.data['type']=='img_bgr':
+			self.img=self.data['content']
+		elif self.data['type']=='img_gray':
+			self.img=cv2.cvtColor(self.data['content'],cv2.COLOR_GRAY2BGR)
 		self.canvas.clear()
+		Clock.schedule_once(partial(self.draw),0.01)
+
+	def draw(self,*args):
+		self.img2texture()
 		with self.canvas:
 			Rectangle(texture=self.texture,
 			pos=(int(self.center_x-self.w_out/2),int(self.center_y-self.h_out/2)),
@@ -50,7 +59,9 @@ class Test(App):
 
 	def build(self):
 		iv=ImageViewer()
-		iv.data={'content':'../../img/face.jpg'}
+		img=cv2.imread('../../img/face.jpg',0)
+		# iv.data={'type':'file_path','content':'../../img/face.jpg'}
+		iv.data={'type':'img_gray','content':img}
 		return iv
 
 
