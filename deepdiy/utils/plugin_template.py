f4a947852bb4e415f8d5fb0da215c55e0f6f6cc2
deepdiy <pubrcv@163.com>
2019-05-14 11:20:44 +1000

    refactor(utils-plugin_template)

--- deepdiy/utils/plugin_template.py
+++ deepdiy/utils/plugin_template.py
@@ -2,21 +2,25 @@ import os,rootpath
 rootpath.append(pattern='plugins')
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import DictProperty
+from kivy.properties import ObjectProperty,DictProperty
 from kivy.uix.boxlayout import BoxLayout
 
 class Demo(BoxLayout):
 	"""docstring for Demo."""
 
+	data=ObjectProperty()
 	bundle_dir = rootpath.detect(pattern='plugins')
-	# Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'gallery.kv')
-	data=DictProperty()
+	# Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'demo.kv')
+
 	def __init__(self):
 		super(Demo, self).__init__()
 
 
 class Test(App):
 	"""docstring for Test."""
+
+	data=ObjectProperty()
+	plugins=DictProperty()
 
 	def __init__(self):
 		super(Test, self).__init__()
