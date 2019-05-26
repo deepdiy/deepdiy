bec115096bf432620b45f329373732e1c9913b13
deepdiy <nie1234567@gmail.com>
2019-05-26 23:21:09 +1000

    refactor(plugin-files):triger add_to_tree by path property

--- deepdiy/plugins/processing/files/files.py
+++ deepdiy/plugins/processing/files/files.py
@@ -3,30 +3,35 @@ rootpath.append(pattern='plugins')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty,StringProperty
 from utils.select_path_dialog import select_file,select_folder
 from utils.get_file_list import get_file_list
 
+
 class Files(BoxLayout):
-	data=ObjectProperty(lambda: None)
-	id='select_path_panel'
+	data=ObjectProperty(BoxLayout())
+	path=StringProperty()
+
 	bundle_dir = rootpath.detect(pattern='plugins')
 	Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'files.kv')
 
+	def __init__(self):
+		super(Files, self).__init__()
+		self.bind(path=self.add_to_tree)
+		self.data.apply_property(files_path=StringProperty('hi'))
+		self.data.bind(files_path=self.setter('path'))
+
 	def open_file(self):
-		path=select_file()
-		self.data['file_list']=[path.replace('/',os.sep)]
-		self.add_to_tree()
+		self.path=select_file()
 
 	def open_folder(self):
-		path=select_folder()
-		if path=='':
-			return
-		self.add_to_tree(path=path)
+		self.path=select_folder()
 
-	def add_to_tree(self,path=''):
-		if path!='':
-			self.data.file_list=get_file_list(path,formats=['jpg','jpeg','bmp','png','tiff','tif'])
+	def add_to_tree(self,*args):
+		print(args)
+		if self.path=='':
+			return
+		self.data.file_list=get_file_list(self.path,formats=['jpg','jpeg','bmp','png','tiff','tif'])
 		tree={'node_id':'resources','children':[],'type':'root'}
 		for file_path in self.data.file_list:
 			tree['children'].append({
@@ -37,12 +42,15 @@ class Files(BoxLayout):
 				'children':[]})
 		self.data.tree=tree
 		self.property('data').dispatch(self)
-		print(path)
+		print(self.path)
 
 
 class Test(App):
 	def build(self):
-		return Files()
+
+		demo=Files()
+		demo.data.files_path='d:/'
+		return demo
 
 if __name__ == '__main__':
 	Test().run()
