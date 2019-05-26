554fca09778db568e12821bf1e6fd6b903938a6e
deepdiy <nie1234567@gmail.com>
2019-05-26 22:00:18 +1000

    fix(utils-get_file_list):fix failure when is file_path instead of dir

--- deepdiy/utils/get_file_list.py
+++ deepdiy/utils/get_file_list.py
@@ -2,6 +2,8 @@ import glob,os
 
 def get_file_list(path,formats=['*']):
 	file_list=[]
+	if not os.path.isdir(path) and path.split('.')[-1]in formats:
+		file_list.append(path)
 	for format in formats:
 		file_list.extend(glob.glob(path + os.sep + '*.'+format))
 	file_list=sorted(file_list,key=len)
@@ -10,5 +12,5 @@ def get_file_list(path,formats=['*']):
 if __name__ == '__main__':
 	path1='../img/'
 	path2='../img/face.jpg'
-	file_list=get_file_list(path1,formats=['jpg'])
+	file_list=get_file_list(path2,formats=['jpg'])
 	print(file_list)
