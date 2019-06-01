c736b1739a27fb137590857b51a364599b569eb9
deepdiy <nie1234567@gmail.com>
2019-06-01 11:30:19 +1000

    refactor(main): simplify code by moving load plugins in thread to plugin_mgr

--- deepdiy/main.py
+++ deepdiy/main.py
@@ -1,34 +1,26 @@
-from core.plugin_mgr import PluginManager
+from core.data_mgr import Data
 from core.widget_mgr import WidgetManager
 from core.display_mgr import DisplayManager
-from core.data_mgr import Data
-from utils.hotkey import Hotkey
+from core.plugin_mgr import PluginManager
 from kivy.app import App
 from kivy.properties import ObjectProperty,DictProperty
-from pebble.concurrent import thread
-from test.debug import *
 
+# from test.debug import *
 
 class MainWindow(App):
 	title='DeepDIY'
-	data=ObjectProperty(lambda: None,force_dispatch=True)
-	plugins=DictProperty()
+	data=ObjectProperty(force_dispatch=True)
+	plugins=DictProperty(force_dispatch=True)
 
 	def __init__(self,**kwargs):
 		super(MainWindow, self).__init__(**kwargs)
-
-	@ thread
-	def load_plugins(self):
+		self.data=Data() # assign data first to reduce dispatching observers
+		self.widget_manager=WidgetManager()
+		self.display_manager=DisplayManager()
 		self.plugin_manager=PluginManager()
 		self.plugin_manager.load_plugins()
-		self.display_manager=DisplayManager()
-		self.data=Data()
-		debug()
 
 	def build(self):
-		self.hotkey=Hotkey()
-		self.widget_manager=WidgetManager()
-		self.load_plugins()
 		return self.widget_manager
 
 if __name__ == '__main__':
