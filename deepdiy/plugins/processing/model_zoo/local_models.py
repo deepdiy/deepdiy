490fcbf5b3e130cec176847637bf70f58233ece7
deepdiy <nie1234567@gmail.com>
2019-06-02 23:39:15 +1000

    feat(plugin-model_zoo-local_models):add popup README

--- deepdiy/plugins/processing/model_zoo/local_models.py
+++ deepdiy/plugins/processing/model_zoo/local_models.py
@@ -1,5 +1,5 @@
 import os,rootpath
-rootpath.append(pattern='plugins')
+rootpath.append(pattern='main.py') # add the directory of main.py to PATH
 import importlib
 from utils.get_file_list import get_file_list
 from kivy.app import App
@@ -9,6 +9,8 @@ from kivy.properties import ObjectProperty,DictProperty,StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.rst import RstDocument
 
 
 class LocalModelCard(BoxLayout):
@@ -18,6 +20,7 @@ class LocalModelCard(BoxLayout):
 	id=StringProperty()
 	tags=StringProperty()
 	abstract=StringProperty()
+	bundle_dir = rootpath.detect(pattern='main.py') # Obtain the dir of main.py
 
 	def __init__(self, **kwargs):
 		super(LocalModelCard, self).__init__()
@@ -26,10 +29,17 @@ class LocalModelCard(BoxLayout):
 		# self.tags=kwargs['tags']
 		self.abstract=kwargs['abstract']
 
+	def popup_readme(self):
+		readme_path=os.sep.join([
+			self.bundle_dir,'plugins','processing','model_zoo','models',self.id,'README.md'])
+		f=open(readme_path).read()
+		description=Popup(title=self.title,size_hint=(None,None),height=500,width=700)
+		description.add_widget(RstDocument(text=f))
+		description.open()
 
 class LocalModels(StackLayout):
 	"""docstring for Run."""
-	bundle_dir = rootpath.detect(pattern='plugins')
+	bundle_dir = rootpath.detect(pattern='main.py') # Obtain the dir of main.py
 	Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'local_models.kv')
 
 	def __init__(self):
@@ -46,9 +56,9 @@ class LocalModels(StackLayout):
 			module_name='.'.join(['plugins','processing','model_zoo','models',name,'api'])
 			module=importlib.import_module(module_name)
 			self.models[name]=getattr(module,'Api')()
-			self.models[name].config_list=get_file_list(model_zoo_dir+os.sep+'models'+os.sep+name+os.sep+'configs')
-			self.models[name].weight_list=get_file_list(model_zoo_dir+os.sep+'weights'+os.sep+name)
-			self.models[name].train_notebooks=get_file_list(model_zoo_dir+os.sep+'models'+os.sep+name+os.sep+'training',formats=['ipynb'])
+			# self.models[name].config_list=get_file_list(model_zoo_dir+os.sep+'models'+os.sep+name+os.sep+'configs')
+			# self.models[name].weight_list=get_file_list(model_zoo_dir+os.sep+'weights'+os.sep+name)
+			# self.models[name].train_notebooks=get_file_list(model_zoo_dir+os.sep+'models'+os.sep+name+os.sep+'training',formats=['ipynb'])
 
 	def render_model_cards(self):
 		for model in self.models:
