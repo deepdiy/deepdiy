db7fb720e824a40d981e24e5d5a514ed671fa47c
deepdiy <nie1234567@gmail.com>
2019-06-03 17:42:55 +1000

    refactor(plugin-model_zoo-local_demos):get demo meta_info from meta.json

--- deepdiy/plugins/processing/model_zoo/local_demos.py
+++ deepdiy/plugins/processing/model_zoo/local_demos.py
@@ -1,6 +1,8 @@
 import os,rootpath
 rootpath.append(pattern='main.py') # add the directory of main.py to PATH
-import importlib,glob
+import importlib
+import glob
+import json
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.factory import Factory
@@ -12,65 +14,44 @@ from kivy.uix.popup import Popup
 from kivy.uix.rst import RstDocument
 from middleware.widget_handler import WidgetHandler
 from middleware.plugin_handler import PluginHandler
-import json
 
 
 class LocalDemoCard(BoxLayout):
 	"""docstring for LocalDemoCard."""
+	demo_id = StringProperty()
+	demo_path = StringProperty()
+	meta_info = DictProperty()
+	bundle_dir=rootpath.detect(pattern='main.py')
 
-	title=StringProperty()
-	id=StringProperty()
-	tags=StringProperty()
-	abstract=StringProperty()
-	demo_path=StringProperty()
-	image_source=StringProperty()
-	bundle_dir=rootpath.detect(pattern='model_zoo')
-
-	def __init__(self,demo_path=''):
+	def __init__(self,demo_id):
 		super(LocalDemoCard, self).__init__()
-		self.demo_path=demo_path
-		self.title=demo_path.split(os.sep)[-1]
-		self.abstract='mrcnn'
-		self.image_source=os.sep.join([self.demo_path,'logo.png'])
+		self.demo_id = demo_id
+		self.demo_path = os.sep.join([self.bundle_dir,'plugins','processing','model_zoo','demos',self.demo_id])
+		self.load_meta_info()
+
+	def load_meta_info(self):
+		meta_path = os.sep.join([
+			self.bundle_dir,'plugins','processing','model_zoo','demos',self.demo_id,'meta.json'])
+		self.meta_info = json.load(open(meta_path))
 
 	def popup_readme(self):
 		f=open(os.sep.join([self.demo_path,'readme.md'])).read()
-		description=Popup(title=self.title,size_hint=(None,None),height=500,width=700)
+		description=Popup(title=self.meta_info['title'],size_hint=(None,None),height=500,width=700)
 		description.add_widget(RstDocument(text=f))
 		description.open()
 
 	def load_demo(self):
 		widget_handler=WidgetHandler()
 		plugin_handler=PluginHandler()
-		json_path=os.sep.join([self.demo_path,'config.json'])
-		demo_config=json.load(open(json_path))
 		widget_handler.switch_screens('processing','predict')
 		plugin_handler.set_plugin_attr(
 			'files','path',os.sep.join([self.demo_path,'images']))
 		plugin_handler.set_plugin_attr(
-			'predict','model_id',demo_config['model'])
+			'predict','model_id',self.meta_info['model'])
 		plugin_handler.set_plugin_attr(
-			'predict','config_id',demo_config['config'])
+			'predict','config_id',self.meta_info['model_config'])
 		plugin_handler.set_plugin_attr(
-			'predict','weight_id',demo_config['weight'])
-		# plugin_handler.set_plugin_attr(
-		# 	'networks','path',os.sep.join([self.demo_path,'images']))
-
-	#
-	# def load_demo(self):
-	# 	app=App.get_running_app()
-	# 	json_path=os.sep.join([self.demo_path,'config.json'])
-	# 	demo_config=json.load(open(json_path))
-	# 	print(demo_config)
-	# 	if hasattr(app,'widget_manager'):
-	# 		app.widget_manager.ids.processing_screens.children[0].children[0]
-	# 		app.widget_manager.ids.processing_screens.current='files'
-	# 		app.widget_manager.ids.processing_screens.children[0].children[0].add_to_tree(os.sep.join([self.demo_path,'images']))
-	# 		app.widget_manager.ids.processing_screens.current='networks'
-	# 		networks=app.widget_manager.ids.processing_screens.children[0].children[0]
-	# 		networks.ids.model_spinner.text=demo_config['model']
-	# 		networks.ids.weight_spinner.text=demo_config['weight']
-	# 		networks.ids.config_spinner.text=demo_config['config']
+			'predict','weight_id',self.meta_info['weight'].split('/')[-1])
 
 
 class LocalDemos(StackLayout):
@@ -84,9 +65,11 @@ class LocalDemos(StackLayout):
 		self.collect_demos()
 
 	def collect_demos(self):
-		demos=glob.glob(os.sep.join([self.bundle_dir,'plugins','processing','model_zoo','demos','*']))
-		for demo in demos:
-			self.add_widget(Factory.LocalDemoCard(demo_path=demo))
+		demos_path_list=glob.glob(os.sep.join([self.bundle_dir,'plugins','processing','model_zoo','demos','*']))
+		for path in demos_path_list:
+			demo_id = path.split(os.sep)[-1]
+			self.add_widget(
+				Factory.LocalDemoCard(demo_id))
 
 
 class Test(App):
