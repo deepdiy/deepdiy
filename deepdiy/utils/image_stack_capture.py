8b9633bf3a378657b18788f937b07a0b87e6a37e
deepdiy <nie1234567@gmail.com>
2019-06-04 16:19:21 +1000

    feat(utils-image_stack_capture):analog of cv2.videoCapture. read tiff file and array

---
+++ deepdiy/utils/image_stack_capture.py
@@ -0,0 +1,73 @@
+import os,rootpath
+rootpath.append(pattern='main.py') # add the directory of main.py to PATH
+import cv2
+import numpy as np
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty,DictProperty,StringProperty,NumericProperty,ListProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class ImageStackCapture(BoxLayout):
+	"""docstring for ImageStackCapture."""
+
+	data = ObjectProperty()
+	stack = ListProperty()
+	path = StringProperty()
+	frame_count = NumericProperty()
+	pos_frames = NumericProperty()
+	bundle_dir = rootpath.detect(pattern='main.py') # Obtain the dir of main.py
+	# Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'demo.kv')
+
+	def __init__(self,path='',stack=[]):
+		super(ImageStackCapture, self).__init__()
+		self.bind(path=self.read_tiff)
+		if path != '':
+			self.path = path
+		if stack != []:
+			self.stack = stack
+
+	def get(self,param):
+		if param == cv2.CAP_PROP_FRAME_COUNT:
+			return self.frame_count
+		if param == cv2.CAP_PROP_POS_FRAMES:
+			return self.pos_frames
+
+	def set(self,param,value):
+		if param == cv2.CAP_PROP_POS_FRAMES:
+			self.pos_frames = int(value)
+
+	def read(self):
+		if self.pos_frames < self.frame_count:
+			frame = self.stack[self.pos_frames]
+			self.pos_frames += 1
+			return True,frame
+		else:
+			return False, None
+
+	def read_tiff(self,*args):
+		ret,self.stack = cv2.imreadmulti(self.path,flags=cv2.IMREAD_ANYDEPTH)
+		self.frame_count = len(self.stack)
+
+
+class Test(App):
+	"""docstring for Test."""
+
+	data=ObjectProperty()
+	plugins=DictProperty()
+
+	def __init__(self):
+		super(Test, self).__init__()
+
+	def build(self):
+		demo=ImageStackCapture()
+		demo.path = 'D:\onedrive\program/for_liuyuan_rotation\data/test.tiff'
+		ret,img = demo.read()
+		img = img/np.max(img)*255
+		img = img.astype(np.uint8)
+		cv2.imshow('img',img)
+		cv2.waitKey(0)
+		# demo.path = 'D:\onedrive\program/for_liuyuan_rotation\data/rotartion/1_X55.tif'
+		return demo
+
+if __name__ == '__main__':
+	Test().run()
