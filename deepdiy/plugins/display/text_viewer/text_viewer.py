14c1663825545cb2f12b1f15305b83c8a4219c29
deepdiy <nie1234567@gmail.com>
2019-06-06 04:19:53 +1000

    refactor(plugin-text_viewer):use pprint to format data content

--- deepdiy/plugins/display/text_viewer/text_viewer.py
+++ deepdiy/plugins/display/text_viewer/text_viewer.py
@@ -1,5 +1,6 @@
 import os,rootpath
 rootpath.append(pattern='main.py') # add the directory of main.py to PATH
+import pprint
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty,DictProperty
@@ -7,21 +8,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.logger import Logger
 
-class Demo(BoxLayout):
-	"""docstring for Demo."""
+class TextViewer(BoxLayout):
+	"""docstring for TextViewer."""
 
 	data=DictProperty()
 	bundle_dir = rootpath.detect(pattern='main.py') # Obtain the dir of main.py
 	# Builder.load_file(bundle_dir +os.sep+'ui'+os.sep+'demo.kv')
 
 	def __init__(self):
-		super(Demo, self).__init__()
+		super(TextViewer, self).__init__()
 		self.text_input=TextInput(id='text_area',readonly=True)
 		self.add_widget(self.text_input)
 		self.bind(data=self.refresh)
 
 	def refresh(self,*args):
-		self.text_input.text = str(self.data['content'])
+		self.text_input.text = pprint.pformat(self.data)
 
 
 class Test(App):
@@ -34,8 +35,9 @@ class Test(App):
 		super(Test, self).__init__()
 
 	def build(self):
-		demo=Demo()
-		demo.data={'content':'abcdefg'}
+		demo=TextViewer()
+		tup = ('spam', ('eggs', ('lumberjack', ('knights', ('ni', ('dead',('parrot', ('fresh fruit',))))))))
+		demo.data={'type':'img','content':'abcdefg','children': ['a' * 10, tup, ['a' * 30, 'b' * 30], ['c' * 20, 'd' * 20]]}
 		return demo
 
 if __name__ == '__main__':
