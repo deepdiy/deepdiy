7e6bae1c7fe4dc2d739bf1678e6a949ce0cdc52e
deepdiy <nie1234567@gmail.com>
2019-07-01 14:36:51 +0800

    refactor(plugin-model_zoo-online_demos):add on_install event

--- deepdiy/plugins/processing/model_zoo/online_demos.py
+++ deepdiy/plugins/processing/model_zoo/online_demos.py
@@ -1,5 +1,5 @@
 import os,rootpath
-rootpath.append(pattern='main.py') # add the directory of main.py to PATH 
+rootpath.append(pattern='main.py') # add the directory of main.py to PATH
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.factory import Factory
@@ -8,6 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.modalview import ModalView
 from kivy.network.urlrequest import UrlRequest
 from utils.downloader import Downloader
+from plugins.processing.model_zoo.installer import Installer
 
 class DemoCard(BoxLayout):
 
@@ -17,20 +18,23 @@ class DemoCard(BoxLayout):
 	abstract=StringProperty()
 	bundle_dir = rootpath.detect(pattern='main.py') # Obtain the dir of main.py
 
-	def __init__(self, **kwargs):
+	def __init__(self, meta_info):
 		super(DemoCard, self).__init__()
-		self.title=kwargs['title']
-		self.id=kwargs['id']
-		# self.tags=kwargs['tags']
-		self.abstract=kwargs['abstract']
-		self.dowload=kwargs['download']
+		self.title=meta_info['title']
+		self.id=meta_info['id']
+		self.abstract=meta_info['abstract']
+		self.meta_info=meta_info
+		self.register_event_type('on_install')
 
 	def install(self,*args):
 		installer=Installer()
-		url=self.dowload
 		self.ids.installer.clear_widgets()
 		self.ids.installer.add_widget(installer)
-		installer.install_demo(self.id,self.dowload)
+		installer.bind(on_success=lambda x:self.dispatch('on_install'))
+		installer.install_demo_full(self.id,self.meta_info)
+
+	def on_install(self,*args):
+		pass
 
 class OnlineDemos(ModalView):
 	"""docstring for OnlineDemos."""
@@ -46,6 +50,7 @@ class OnlineDemos(ModalView):
 		self.collect_local_demos()
 		self.collect_online_demos()
 		self.bind(online_demos=self.insert_model_cards)
+		self.register_event_type('on_install')
 
 	def collect_local_demos(self):
 		pass
@@ -54,9 +59,13 @@ class OnlineDemos(ModalView):
 		req = UrlRequest('http://www.deepdiy.net/model_zoo/api/demo_list.json', lambda req,result:setattr(self,'online_demos',result))
 
 	def insert_model_cards(self,*args):
-		for model in self.online_demos:
-			self.ids.model_album.add_widget(Factory.DemoCard(
-				title=model['title'],id=model['id'],abstract=model['abstract'],download=model['download']))
+		for meta_info in self.online_demos:
+			card = Factory.DemoCard(meta_info)
+			self.ids.model_album.add_widget(card)
+			card.bind(on_install=lambda x:self.dispatch('on_install'))
+
+	def on_install(self,*args):
+		pass
 
 
 class Test(App):
