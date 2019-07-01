703409f823cb7d0e3ebdb1b7fea6b851abe8612f
deepdiy <nie1234567@gmail.com>
2019-07-01 14:59:31 +0800

    chore(plugin-debugger-load_demo):add import Clock

--- deepdiy/plugins/system/debugger/load_demo/load_demo.py
+++ deepdiy/plugins/system/debugger/load_demo/load_demo.py
@@ -2,6 +2,8 @@ import os,rootpath
 rootpath.append(pattern='main.py') # add the directory of main.py to PATH
 from middleware.widget_handler import WidgetHandler
 from middleware.plugin_handler import PluginHandler
+from kivy.app import App
+from kivy.clock import Clock
 
 def run():
 	widget_handler=WidgetHandler()
@@ -10,6 +12,6 @@ def run():
 	bundle_dir=rootpath.detect(pattern='main.py')
 	plugin_handler.set_plugin_attr(
 		'files','path',os.sep.join([bundle_dir,'img','face.jpg']))
-	plugin_handler.data.select_idx = [0,0]
+	# plugin_handler.data.select_idx = [0,0]
 
 run()
